1bd67b8827e9541615ad6b4b8845291da8a8e3cf
hyeonic <evan3566@gmail.com>
2021-03-24 19:47:19 +0900

    feat: 개발환경 구축
    Spring boot 2.4.4
    spring web
    spring data jpa
    spring boot devtools
    gradle
    java 11
    h2 database 1.4.200
    h2 설정을 의한 application.yml 파일 추가
    h2 DB 연동 확인

---
+++ src/test/java/com/skhuedin/skhuedin/SkhuedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhuedin.skhuedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuedInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
