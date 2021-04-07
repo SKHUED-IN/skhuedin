cc2a8289687c1d9d634972c109265e907a9bff71
her0807 <her0807@naver.com>
2021-04-07 15:03:09 +0900

    feat: kakao 로그인 구현 (임시 커밋)
    카카오 로그인 구현 중, 팀원이 PR 후 병합을 완료 했기 때문에
    가장 최신 버전으로 업그레이드해서 변경된 사항으로
    작업을 진행하기 위해 임시 커밋을 시도함.

---
+++ src/main/java/com/skhuedin/skhuedin/controller/UserController.java
@@ -0,0 +1,107 @@
+package com.skhuedin.skhuedin.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skhuedin.skhuedin.domain.KakaoProfile;
+import com.skhuedin.skhuedin.domain.OAuthToken;
+import com.skhuedin.skhuedin.domain.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody
+    String kakaoCallback(String code) {
+
+        //HttpHeader 오브젝트 생성
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "f4f3a16b864f2046d38669b4f8a2a482");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+
+        // Http 요청하기 - post 방식으로ㅡ 그리고 응답받음.
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = null;
+
+        try {
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        //HttpHeader 오브젝트 생성
+        RestTemplate rt2 = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //HttpBody 오브젝트 생성
+
+        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
+                new HttpEntity<>(headers2);
+
+        // Http 요청하기 - post 방식으로ㅡ 그리고 응답받음.
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest2,
+                String.class
+        );
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoProfile kakaoProfile =null;
+
+        try {
+            kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("카카오 아이디: "+ kakaoProfile.getId());
+        System.out.println("카카오 email: "+ kakaoProfile.getKakao_account().getEmail());
+
+        System.out.println("skhuedin 유저네임: "+ kakaoProfile.getKakao_account().getEmail()+"_"+kakaoProfile.getId());
+        System.out.println("skhuedin email: "+ kakaoProfile.getKakao_account().getEmail());
+
+        UUID password = UUID.randomUUID();
+
+        System.out.println("skhuedin password: "+password );
+        //User에 들어가는 내용 : name, password, email
+
+        User user =User.builder()
+
+                .email(kakaoProfile.getKakao_account().getEmail())
+                build();
+
+        return response2.getBody();
+    }
+}
---
+++ src/main/java/com/skhuedin/skhuedin/domain/OAuthToken.java
@@ -0,0 +1,17 @@
+package com.skhuedin.skhuedin.domain;
+
+import lombok.Data;
+
+@Data
+public class OAuthToken {
+
+    /*
+    기존의 문법과 어긋나는 변수명이지만, 카카오에서 리스폰즈로 오는 값의 이름과 통일 시키기 위함임.
+     */
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private int expires_in;
+    private String scope;
+    private int refresh_token_expires_in;
+}
