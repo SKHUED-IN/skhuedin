f3de90760eb3c74e9068a8a06b55403beb208210
her0807 <her0807@naver.com>
2021-04-09 16:00:27 +0900

    feat: Service 역할 분리 기존에 controller 에 있던 로직을 service 로 옮겼음.

--- src/main/java/com/skhuedin/skhuedin/social/kakao/KakaoController.java
+++ src/main/java/com/skhuedin/skhuedin/social/kakao/KakaoController.java
@@ -1,118 +1,28 @@
 package com.skhuedin.skhuedin.social.kakao;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.skhuedin.skhuedin.domain.Provider;
 import com.skhuedin.skhuedin.domain.User;
-import com.skhuedin.skhuedin.service.UserService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.client.RestTemplate;
-
-import java.util.UUID;
-
 
 @Controller
 @RequiredArgsConstructor
 public class KakaoController {
-    private final UserService userService;
+    private final KakaoService kakaoService;
 
     @GetMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
+    public String main() {
         return "kakaoLogin";
     }
 
     @GetMapping("/auth/kakao/callback")
     public String kakaoCallback(String code, Model model) {
-
-        //HttpHeader 오브젝트 생성
-        RestTemplate rt = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        //HttpBody 오브젝트 생성
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", "f4f3a16b864f2046d38669b4f8a2a482");
-        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
-        params.add("code", code);
-
-        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-                new HttpEntity<>(params, headers);
-
-        // Http 요청하기 - post 방식으로ㅡ 그리고 응답받음.
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        OAuthToken oauthToken = null;
-
-        try {
-            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        //HttpHeader 오브젝트 생성
-        RestTemplate rt2 = new RestTemplate();
-        HttpHeaders headers2 = new HttpHeaders();
-        headers2.add("Authorization", "Bearer " + oauthToken.getAccess_token());
-        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
-                new HttpEntity<>(headers2);
-
-        // Http 요청하기 - post 방식으로ㅡ 그리고 응답받음.
-        ResponseEntity<String> response2 = rt2.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoProfileRequest2,
-                String.class
-        );
-
-        ObjectMapper objectMapper2 = new ObjectMapper();
-        KakaoProfile kakaoProfile = null;
-
-        try {
-            kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        UUID password = UUID.randomUUID(); // 임시 비밀번호
-
-        User user = User.builder()
-                .email(kakaoProfile.getKakao_account().getEmail())
-                .name(kakaoProfile.getKakao_account().getProfile().getNickname())
-                .provider(Provider.KAKAO)
-                .userImageUrl(kakaoProfile.getProperties().getProfile_image())
-                .password(password.toString())
-                .entranceYear(null)
-                .graduationYear(null)
-                .build();
-
-        userService.join(user);
-
+        User user = kakaoService.saveKakaoUser(kakaoService.getProfile(
+                kakaoService.oAuthToken(code)));
         model.addAttribute("user", user);
-
         return "kakaoProfile";
     }
 }
