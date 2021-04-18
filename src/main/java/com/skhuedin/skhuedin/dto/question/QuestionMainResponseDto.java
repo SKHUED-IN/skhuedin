80b5b7f73d47809262f04f862b5c0ad39f6b0b15
hyeonic <evan3566@gmail.com>
2021-04-18 21:46:58 +0900

    feat: question 조회 기능 수정
    question 조회 기능에 comments 조회 기능 추가

--- src/main/java/com/skhuedin/skhuedin/dto/question/QuestionMainResponseDto.java
+++ src/main/java/com/skhuedin/skhuedin/dto/question/QuestionMainResponseDto.java
@@ -1,10 +1,13 @@
 package com.skhuedin.skhuedin.dto.question;
 
 import com.skhuedin.skhuedin.domain.Question;
+import com.skhuedin.skhuedin.dto.comment.CommentMainResponseDto;
 import com.skhuedin.skhuedin.dto.user.UserMainResponseDto;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class QuestionMainResponseDto {
@@ -17,6 +20,7 @@ public class QuestionMainResponseDto {
     private Boolean status;
     private Boolean fix;
     private Integer view;
+    private List<CommentMainResponseDto> comments = new ArrayList<>();
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
@@ -29,6 +33,20 @@ public class QuestionMainResponseDto {
         this.status = question.getStatus();
         this.fix = question.getFix();
         this.view = question.getView();
+        this.createdDate = question.getCreatedDate();
+        this.lastModifiedDate = question.getLastModifiedDate();
+    }
+
+    public QuestionMainResponseDto(Question question, List<CommentMainResponseDto> comments) {
+        this.id = question.getId();
+        this.targetUser = new UserMainResponseDto(question.getTargetUser());
+        this.writerUser = new UserMainResponseDto(question.getWriterUser());
+        this.title = question.getTitle();
+        this.content = question.getContent();
+        this.status = question.getStatus();
+        this.fix = question.getFix();
+        this.view = question.getView();
+        this.comments = comments;
         this.createdDate = question.getCreatedDate();
         this.lastModifiedDate = question.getLastModifiedDate();
     }
