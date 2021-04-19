9bf3817512b17ff70156146c59c416af45b9bd8f
her0807 <her0807@naver.com>
2021-04-19 19:19:51 +0900

    feat: social 공통 양식으로 수정 1) 기존에 구현했던 코드를 공통 양식에 맞추어 수정함.

---
+++ src/main/java/com/skhuedin/skhuedin/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.skhuedin.skhuedin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String main() {
+        return "kakaoLogin";
+    }
+}
