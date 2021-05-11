b321d291df30b8e141fe582694b7c17f250b6195
her0807 <her0807@naver.com>
2021-05-11 22:41:42 +0900

    feat: google temp commit

--- src/main/java/com/skhuedin/skhuedin/social/google/GoogleInnerProfile.java
+++ src/main/java/com/skhuedin/skhuedin/social/google/GoogleInnerProfile.java
@@ -1,41 +1,17 @@
 package com.skhuedin.skhuedin.social.google;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
-@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GoogleInnerProfile {
-
-    public GoogleInnerProfile(String iss, String azp, String aud, String sub, String atHash,
-                              String name, String picture, String givenName, String familyName,
-                              String locale, String iat, String exp, String alg, String kid, String typ, String email,
-                              Map<String, Object> additionalProperties) {
-        this.iss = iss;
-        this.azp = azp;
-        this.aud = aud;
-        this.sub = sub;
-        this.atHash = atHash;
-        this.name = name;
-        this.picture = picture;
-        this.givenName = givenName;
-        this.familyName = familyName;
-        this.locale = locale;
-        this.iat = iat;
-        this.exp = exp;
-        this.alg = alg;
-        this.kid = kid;
-        this.typ = typ;
-        this.additionalProperties = additionalProperties;
-        this.email = email;
-    }
-
-    public GoogleInnerProfile() {
-
-    }
 
     public String iss;
     public String azp;
@@ -51,6 +27,28 @@ public class GoogleInnerProfile {
     public String exp;
     public String alg;
     public String kid;
+
+    @Builder
+    public GoogleInnerProfile(String iss, String azp, String aud, String sub, String atHash, String name, String picture, String givenName, String familyName, String locale, String iat, String exp, String alg, String kid, String typ, String email, Map<String, Object> additionalProperties) {
+        this.iss = iss;
+        this.azp = azp;
+        this.aud = aud;
+        this.sub = sub;
+        this.atHash = atHash;
+        this.name = name;
+        this.picture = picture;
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.locale = locale;
+        this.iat = iat;
+        this.exp = exp;
+        this.alg = alg;
+        this.kid = kid;
+        this.typ = typ;
+        this.email = email;
+        this.additionalProperties = additionalProperties;
+    }
+
     public String typ;
     public String email;
 
