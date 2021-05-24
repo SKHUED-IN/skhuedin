3e50ec53fed758cf4c11acf8bcf0af884ac50486
hyeonic <evan3566@gmail.com>
2021-05-24 18:47:31 +0900

    feat: 외부에 profile 정보를 저장하기 위해 수정
    window 기준으로 작성

--- src/main/java/com/skhuedin/skhuedin/config/WebMvcConfig.java
+++ src/main/java/com/skhuedin/skhuedin/config/WebMvcConfig.java
@@ -1,14 +1,23 @@
 package com.skhuedin.skhuedin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final HandlerInterceptor bearerAuthInterceptor;
+
+    @Value("${resources.location}")
+    private String resourcesLocation;
+
+    @Value("${resources.uri_path}")
+    private String resourcesUriPath;
 
     public WebMvcConfig(HandlerInterceptor bearerAuthInterceptor) {
         this.bearerAuthInterceptor = bearerAuthInterceptor;
@@ -25,5 +34,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcesUriPath + "/**")
+                .addResourceLocations("file://" + resourcesLocation);
     }
 }
