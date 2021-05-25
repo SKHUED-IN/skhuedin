372d359aa2a7cfea367a0595c66a0f46035be5ab
her0807 <her0807@naver.com>
2021-05-26 01:29:18 +0900

    feat : google dto 수정

--- src/main/java/com/skhuedin/skhuedin/social/google/GoogleProfile.java
+++ src/main/java/com/skhuedin/skhuedin/social/google/GoogleProfile.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class GoogleProfile {
     String id;
+    String email;
+    boolean verified_email;
     String name;
     String given_name;
     String family_name;
@@ -17,8 +19,10 @@ public class GoogleProfile {
     String locale;
 
     @Builder
-    public GoogleProfile(String id, String name, String given_name, String family_name, String picture, String locale) {
+    public GoogleProfile(String id, String email, boolean verified_email, String name, String given_name, String family_name, String picture, String locale) {
         this.id = id;
+        this.email = email;
+        this.verified_email = verified_email;
         this.name = name;
         this.given_name = given_name;
         this.family_name = family_name;
