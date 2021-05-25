8ea97445a6cd19556d6feb461bfde3f20a4f9027
her0807 <her0807@naver.com>
2021-05-25 18:18:42 +0900

    feat: user updateDto 로직 수정 1) 불필요한 필드 제거

--- src/main/java/com/skhuedin/skhuedin/dto/user/UserUpdateDto.java
+++ src/main/java/com/skhuedin/skhuedin/dto/user/UserUpdateDto.java
@@ -4,16 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @Builder
 public class UserUpdateDto {
-
-    @NotNull(message = "user의 id는 null이 될 수 없습니다.")
-    private Long id;
 
     @Size(max = 4, min = 4, message = "entranceYear는 4자리여야 합니다.")
     private String entranceYear;
