49dcf855a35f7ad8ee1f5028ba223332530b8a26
hyeonic <evan3566@gmail.com>
2021-05-29 01:10:12 +0900

    feat: 자체 token validation 추가
    token 값이 유효한지 확인하는 validation 추가

--- src/main/java/com/skhuedin/skhuedin/controller/response/CommonResponse.java
+++ src/main/java/com/skhuedin/skhuedin/controller/response/CommonResponse.java
@@ -4,22 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class CommonResponse<T> extends BasicResponse {
 
     private T data;
-    private int count;
 
     public CommonResponse(T data) {
         this.data = data;
-        if (data instanceof List) {
-            this.count = ((List<?>) data).size();
-        } else {
-            this.count = 1;
-        }
     }
 }
---
+++ src/main/java/com/skhuedin/skhuedin/dto/user/UserTokenValidationDto.java
@@ -0,0 +1,13 @@
+package com.skhuedin.skhuedin.dto.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserTokenValidationDto {
+
+    String token;
+}
