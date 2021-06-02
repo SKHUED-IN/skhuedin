ff2515a4eb4a89fcdfa908e195fc352e35d9db44
hyeonic <evan3566@gmail.com>
2021-06-02 12:29:28 +0900

    feat: 검색 input 및 조건 select 추가
    검색 조건을 선택하고 keyword를 검색할 수 있도록 구현

--- src/main/java/com/skhuedin/skhuedin/controller/admin/api/AdminApiController.java
+++ src/main/java/com/skhuedin/skhuedin/controller/admin/api/AdminApiController.java
@@ -6,24 +6,40 @@ import com.skhuedin.skhuedin.infra.MyRole;
 import com.skhuedin.skhuedin.infra.Role;
 import com.skhuedin.skhuedin.service.PostsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@Slf4j
 public class AdminApiController {
 
     private final PostsService postsService;
 
 //    @MyRole(role = Role.ADMIN)
     @GetMapping("posts")
-    public ResponseEntity<? extends BasicResponse> getPosts(Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new CommonResponse<>(postsService.findAll(pageable)));
+    public ResponseEntity<? extends BasicResponse> getPosts(
+            Pageable pageable,
+            @RequestParam(name = "category", defaultValue = "") String categoryName,
+            @RequestParam(name = "username", defaultValue = "") String username) {
+
+        if (categoryName.isEmpty() && !username.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new CommonResponse<>(postsService.findByUserName(pageable, username)));
+        } else if (!categoryName.isEmpty() && username.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new CommonResponse<>(postsService.findByCategoryName(pageable, categoryName)));
+        } else {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new CommonResponse<>(postsService.findAll(pageable)));
+        }
     }
 }
