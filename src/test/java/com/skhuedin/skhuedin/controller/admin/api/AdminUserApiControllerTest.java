945620e7fcff1a7163d2166581f50e64c66f6e92
her0807 <her0807@naver.com>
2021-06-04 02:41:22 +0900

    feat: test code 작성 1) 테스트 코드를 작성함

---
+++ src/test/java/com/skhuedin/skhuedin/controller/admin/api/AdminUserApiControllerTest.java
@@ -0,0 +1,51 @@
+package com.skhuedin.skhuedin.controller.admin.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skhuedin.skhuedin.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql("/truncate.sql")
+@SpringBootTest
+class AdminUserApiControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @DisplayName("user 검색 없이 전체 조회")
+    @Test
+    void user_find_role() throws Exception {
+        //given 어떤 값이 주어지고
+
+        //when 무엇을 했을 때 then 어떤 값을 원한다.
+        mockMvc.perform(get("/api/admin/users?page=0&size=10")
+                .param("role", "ADMIN")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+    }
+}
