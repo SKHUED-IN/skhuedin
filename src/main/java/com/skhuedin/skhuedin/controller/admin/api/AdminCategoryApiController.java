cb7accacd31cd7a18eee8f9367bf6ee50136a6bb
hyeonic <evan3566@gmail.com>
2021-06-07 19:42:20 +0900

    fix: fragments 경로로 인한 버그 수정

--- src/main/java/com/skhuedin/skhuedin/controller/admin/api/AdminCategoryApiController.java
+++ src/main/java/com/skhuedin/skhuedin/controller/admin/api/AdminCategoryApiController.java
@@ -3,9 +3,9 @@ package com.skhuedin.skhuedin.controller.admin.api;
 import com.skhuedin.skhuedin.controller.response.BasicResponse;
 import com.skhuedin.skhuedin.controller.response.CommonResponse;
 import com.skhuedin.skhuedin.dto.category.CategoryRequestDto;
-import com.skhuedin.skhuedin.dto.user.UserAdminUpdateRequestDto;
+import com.skhuedin.skhuedin.infra.MyRole;
+import com.skhuedin.skhuedin.infra.Role;
 import com.skhuedin.skhuedin.service.CategoryService;
-import com.skhuedin.skhuedin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminCategoryApiController {
     private final CategoryService categoryService;
 
-    //@MyRole(role = Role.ADMIN)
-    @GetMapping("/category")
+//    @MyRole(role = Role.ADMIN)
+    @GetMapping("category")
     public ResponseEntity<? extends BasicResponse> getCategory(
             Pageable pageable,
             @RequestParam(name = "cmd", defaultValue = "") String cmd
@@ -44,32 +43,32 @@ public class AdminCategoryApiController {
         }
     }
 
-    //    //    @MyRole(role = Role.ADMIN)
-    @GetMapping("/category/{categoryId}")
+//    @MyRole(role = Role.ADMIN)
+    @GetMapping("category/{categoryId}")
     public ResponseEntity<? extends BasicResponse> getCategory(@PathVariable("categoryId") Long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new CommonResponse<>(categoryService.findByIdByAdmin(id)));
     }
 
-    @GetMapping("/category/up/{categoryId}")
+    @GetMapping("category/up/{categoryId}")
     public ResponseEntity<? extends BasicResponse> upCategory(@PathVariable("categoryId") Long id) {
         categoryService.addWeight(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @GetMapping("/category/down/{categoryId}")
+    @GetMapping("category/down/{categoryId}")
     public ResponseEntity<? extends BasicResponse> downCategory(@PathVariable("categoryId") Long id) {
         categoryService.subtractWeight(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PostMapping("/category")
+    @PostMapping("category")
     public ResponseEntity<? extends BasicResponse> createCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
         categoryService.save(categoryRequestDto);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @DeleteMapping("/category/{categoryId}")
+    @DeleteMapping("category/{categoryId}")
     public ResponseEntity<? extends BasicResponse> deleteUsers(@PathVariable("categoryId") Long id) {
         categoryService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
